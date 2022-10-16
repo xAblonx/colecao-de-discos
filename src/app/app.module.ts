739a61facab6c9e65a1dcb5cc24f4310d72836cf
Rennan Marcos Balan <rennanbalan@gmail.com>
2022-10-16 12:39:35 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LinksComponent } from './links/links.component';
+import { NextStepsComponent } from './next-steps/next-steps.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { HighlightCardComponent } from './highlight-card/highlight-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LinksComponent,
+    NextStepsComponent,
+    ResourcesComponent,
+    HighlightCardComponent
   ],
   imports: [
     BrowserModule
